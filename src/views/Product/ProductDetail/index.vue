4e01093abe1d79c075d2a591b09e0c05c50202ab
mingyeongwon <wonmingy@gmail.com>
2024-06-05 21:09:59 +0900

    edited detail

--- src/views/Product/ProductDetail/index.vue
+++ src/views/Product/ProductDetail/index.vue
@@ -1,12 +1,58 @@
 <template>
     <div>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <img src="https://photos.zillowstatic.com/fp/f95a407a00e51654545dbec4d9a5400f-cc_ft_768.webp">
+                </div>
+                <div class="col">
+                    <img src="https://photos.zillowstatic.com/fp/17c649bba13d15df3cccf0b91b52d9e0-cc_ft_768.webp">
+                </div>
+                <div class="col">
+                    <img src="https://photos.zillowstatic.com/fp/2ab0729fe00ec391ecb064b786b102eb-cc_ft_768.webp">
+                </div>
+                <div class="col">
+                    <img src="https://photos.zillowstatic.com/fp/bcd02478396f1bc38c713a0b87e85c9b-cc_ft_768.webp">
+                </div>
+                <div class="col">
+                    <img src="https://photos.zillowstatic.com/fp/506243417f2983d94dcf8deecbee10d3-cc_ft_768.webp">
+                </div>
+            </div>
+        </div>
         <div>
-            <div class="photoContainer">
-                <div class="first"><img src="https://photos.zillowstatic.com/fp/f95a407a00e51654545dbec4d9a5400f-cc_ft_768.webp" alt=""></div>
-                <div><img src="https://photos.zillowstatic.com/fp/17c649bba13d15df3cccf0b91b52d9e0-cc_ft_768.webp" alt=""></div>
-                <div><img src="https://photos.zillowstatic.com/fp/a2805e62f01b4be3d99c50f76eaf8cc4-cc_ft_768.webp" alt=""></div>
-                <div><img src="https://photos.zillowstatic.com/fp/2ab0729fe00ec391ecb064b786b102eb-cc_ft_768.webp" alt=""></div>
-                <div><img src="https://photos.zillowstatic.com/fp/bcd02478396f1bc38c713a0b87e85c9b-cc_ft_768.webp" alt=""></div>
+            <div>
+                <div>
+                    <h1>가격정보</h1>
+                </div>
+                <div>
+                    <p>전세</p>
+                    <p>1억5000</p>
+                </div>
+                <div>
+                    <p>관리비</p>
+                    <p>매월 5만원</p>
+                </div>
+                <div>
+                    <p>주차가능여부</p>
+                    <p>가능</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h1>가격정보</h1>
+                </div>
+                <div>
+                    <p>전세</p>
+                    <p>1억5000</p>
+                </div>
+                <div>
+                    <p>관리비</p>
+                    <p>매월 5만원</p>
+                </div>
+                <div>
+                    <p>주차가능여부</p>
+                    <p>가능</p>
+                </div>
             </div>
         </div>
     </div>
@@ -17,7 +63,17 @@
   </script>
   
   <style scoped>
+    /* .content {
+        display: flex;
+ 
+    }
+
   .photoContainer {
+    width: 1200px;
+    margin: 0px auto;
+  }
+
+  .photos {
     display: grid;
     grid-template-columns: 586px minmax(0px, 1fr) minmax(0px, 1fr);
     grid-template-rows: repeat(2, minmax(0px, 1fr));
@@ -28,5 +84,5 @@
 
   .first {
     grid-row: 1 / 3;
-  }
+  } */
   </style>
