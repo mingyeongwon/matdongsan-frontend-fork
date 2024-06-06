3bb4c6ba2e9ce652e5443e0512c47fecb19d4e6f
mingyeongwon <wonmingy@gmail.com>
2024-06-07 08:55:38 +0900

    edited detail page

--- src/views/Product/ProductDetail/index.vue
+++ src/views/Product/ProductDetail/index.vue
@@ -2,57 +2,179 @@
     <div>
         <div class="container">
             <div class="row">
-                <div class="col">
-                    <img src="https://photos.zillowstatic.com/fp/f95a407a00e51654545dbec4d9a5400f-cc_ft_768.webp">
+                <div class="col-6">
+                    <img class="img-fluid" src="https://photos.zillowstatic.com/fp/f95a407a00e51654545dbec4d9a5400f-cc_ft_768.webp">
                 </div>
-                <div class="col">
-                    <img src="https://photos.zillowstatic.com/fp/17c649bba13d15df3cccf0b91b52d9e0-cc_ft_768.webp">
-                </div>
-                <div class="col">
-                    <img src="https://photos.zillowstatic.com/fp/2ab0729fe00ec391ecb064b786b102eb-cc_ft_768.webp">
-                </div>
-                <div class="col">
-                    <img src="https://photos.zillowstatic.com/fp/bcd02478396f1bc38c713a0b87e85c9b-cc_ft_768.webp">
-                </div>
-                <div class="col">
-                    <img src="https://photos.zillowstatic.com/fp/506243417f2983d94dcf8deecbee10d3-cc_ft_768.webp">
+                <div class="col-6">
+                    <div class="row">
+                        <div class="col">
+                            <img class="img-fluid" src="https://photos.zillowstatic.com/fp/17c649bba13d15df3cccf0b91b52d9e0-cc_ft_768.webp">
+                        </div>
+                        <div class="col">
+                            <img class="img-fluid" src="https://photos.zillowstatic.com/fp/2ab0729fe00ec391ecb064b786b102eb-cc_ft_768.webp">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <img class="img-fluid" src="https://photos.zillowstatic.com/fp/bcd02478396f1bc38c713a0b87e85c9b-cc_ft_768.webp">
+                        </div>
+                        <div class="col">
+                            <img class="img-fluid" src="https://photos.zillowstatic.com/fp/506243417f2983d94dcf8deecbee10d3-cc_ft_768.webp">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div>
-            <div>
-                <div>
-                    <h1>가격정보</h1>
-                </div>
-                <div>
-                    <p>전세</p>
-                    <p>1억5000</p>
-                </div>
-                <div>
-                    <p>관리비</p>
-                    <p>매월 5만원</p>
-                </div>
-                <div>
-                    <p>주차가능여부</p>
-                    <p>가능</p>
-                </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-9">
+                <section>
+                    <div>
+                        <h1>가격정보</h1>
+                    </div>
+                    <ul>
+                        <li>
+                            <div><p>전세</p></div>
+                            <div><p>1억5000</p></div>
+                        </li>
+                        <li>
+                            <div><p>관리비</p></div>
+                            <div><p>매월 5만원</p></div>
+                        </li>
+                        <li>
+                            <div><p>주차가능여부</p></div>
+                            <div><p>가능</p></div>
+                        </li>
+                    </ul>
+                </section>
+                <section>
+                    <div>
+                        <h1>상세정보</h1>
+                    </div>
+                    <ul>
+                        <li>
+                            <div><p>방종류</p></div>
+                            <div><p>투룸</p></div>
+                        </li>
+                        <li>
+                            <div><p>해당층/건물층</p></div>
+                            <div><p>4층/6층</p></div>
+                        </li>
+                        <li>
+                            <div><p>전용/공급면적</p></div>
+                            <div><p>29.24m2/34.87m2</p></div>
+                        </li>
+                        <li>
+                            <div><p>방 수/욕실 수</p></div>
+                            <div><p>2개/1개</p></div>
+                        </li>
+                        <li>
+                            <div><p>방향</p></div>
+                            <div><p>남동 (안방 기준)</p></div>
+                        </li>
+                        <li>
+                            <div><p>난방종류</p></div>
+                            <div><p>개별난방</p></div>
+                        </li>
+                        <li>
+                            <div><p>엘리베이터</p></div>
+                            <div><p>있음</p></div>
+                        </li>
+                        <li>
+                            <div><p>총 주차대수</p></div>
+                            <div><p>10대</p></div>
+                        </li>
+                        <li>
+                            <div><p>입주가능일</p></div>
+                            <div><p>즉시 입주 (협의 가능)</p></div>
+                        </li>
+                        <li>
+                            <div><p>건축물용도</p></div>
+                            <div><p>공동주택</p></div>
+                        </li>
+                        <li>
+                            <div><p>최초등록일</p></div>
+                            <div><p>2024.05.14</p></div>
+                        </li>
+                    </ul>
+                </section>
+                <section>
+                    <div>
+                        <h1>옵션</h1>
+                    </div>
+                </section>
+                <section>
+                    <div>
+                        <h1>위치 및 주변시설</h1>
+                    </div>
+                </section>
+                <section>
+                    <div>
+                        <h1>상세설명</h1>
+                    </div>
+                </section>
+                <section>
+                    <div>
+                        <h1>다른 방 더보기</h1>
+                    </div>
+                </section>
+                <section>
+                    <div>
+                        <h1>허위매물 신고하기</h1>
+                    </div>
+                </section>
             </div>
-            <div>
+            <aside class="col-3">
                 <div>
-                    <h1>가격정보</h1>
+                    <div>
+                        <div class="productNum"><p>매물번호 43995011</p></div>
+                        <div>
+                            <p>전세 1억5000</p>
+                            <p>최근 7일 매물 조회 수 340회</p>
+                        </div>
+                        <ul>
+                            <li class="d-flex">
+                                <div>
+                                    <img width="24" height="24" src="https://cdn-icons-png.flaticon.com/512/510/510965.png" alt="">
+                                    <span>오피스텔</span>
+                                </div>
+                                <div>
+                                    <img width="24" height="24" src="https://cdn.icon-icons.com/icons2/1491/PNG/512/ruler_102796.png" alt="">
+                                    <span>20.98m2</span>
+                                </div>
+                            </li>
+                            <li class="d-flex">
+                                <div>
+                                    <img width="24" height="24" src="https://cdn-icons-png.flaticon.com/256/637/637209.png" alt="">
+                                    <span>8층/17층</span>
+                                </div>
+                                <div>
+                                    <img width="24" height="24" src="https://cdn-icons-png.flaticon.com/512/2693/2693796.png" alt="">
+                                    <span>19만 원</span>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <p>방/욕실</p>
+                                    <p>2개/1개</p>
+                                </div>
+                                <div>
+                                    <p>위치</p>
+                                    <p>서울특별시 강서구 방화동</p>
+                                </div>
+                            </li>
+                        </ul>
+                        <div></div>
+                        <div></div>
+                        <div>
+                            <button class="btn">연락처 보기</button>
+                            <button class="btn">
+                                <img src="">
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <p>전세</p>
-                    <p>1억5000</p>
-                </div>
-                <div>
-                    <p>관리비</p>
-                    <p>매월 5만원</p>
-                </div>
-                <div>
-                    <p>주차가능여부</p>
-                    <p>가능</p>
-                </div>
+            </aside>
             </div>
         </div>
     </div>
@@ -63,26 +185,11 @@
   </script>
   
   <style scoped>
-    /* .content {
-        display: flex;
- 
+    ul {
+        list-style: none;
     }
 
-  .photoContainer {
-    width: 1200px;
-    margin: 0px auto;
-  }
-
-  .photos {
-    display: grid;
-    grid-template-columns: 586px minmax(0px, 1fr) minmax(0px, 1fr);
-    grid-template-rows: repeat(2, minmax(0px, 1fr));
-    gap: 8px;
-    width: 100%;
-    height: 440px;
-  }
-
-  .first {
-    grid-row: 1 / 3;
-  } */
+    .productNum {
+        border: 1px solid rgb(101, 101, 101);
+    }
   </style>
