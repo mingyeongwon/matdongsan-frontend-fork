f992bf93ce6de4fe0ab8330552e7ff14f88af07d
mingyeongwon <wonmingy@gmail.com>
2024-06-07 21:05:49 +0900

    added info

---
+++ src/views/MyPage/MyInfo/Modal.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="modal">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">
+                제목
+            </h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <p>다이얼로그 내용</p>
+          </div>
+          <div class="modal-footer">
+              <button type="button" class="btn btn-info btn-sm" data-bs-dismiss="modal">확인</button>
+              <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="modal">닫기</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
