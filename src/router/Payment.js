2bddb0f9f2e8dcf81b8f7d762bbd3d919cd066d7
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-06-09 18:41:02 +0900

    removed order confirm page

--- src/router/Payment.js
+++ src/router/Payment.js
@@ -5,13 +5,6 @@ const routes = [
       import(/* webpackChunkName: "Payment" */ "@/views/Payment/Payment"),
   },
   {
-    path: "/Payment/OrderConfirmation",
-    component: () =>
-      import(
-        /* webpackChunkName: "Payment" */ "@/views/Payment/OrderConfirmation"
-      ),
-  },
-  {
     path: "/Payment/PaymentInfo",
     component: () =>
       import(/* webpackChunkName: "Payment" */ "@/views/Payment/PaymentInfo"),
