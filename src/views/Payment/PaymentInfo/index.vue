482817b262252b74933393bd7288f8e2da2f4005
mingyeongwon <wonmingy@gmail.com>
2024-06-09 18:13:56 +0900

    edited router

---
+++ src/views/Payment/PaymentInfo/index.vue
@@ -0,0 +1,66 @@
+<template>
+<div>
+    <div><h1>상품 소개</h1></div>
+    <div class="productForm-box">
+        <div>
+            <h1>
+                <p>원하는 매물을</p>
+                <p>등록할 수 있는 일반 상품</p>
+            </h1>
+            <p>원룸, 투룸, 오피스텔 등 원하는 매물을 등록할 수 있습니다.</p>
+            <p>월 300만명이 부동산을 검색하는 집이지에 매물을 노출하세요.</p>
+        </div>
+    </div>
+    <div class="advertisement-box">
+        <div>
+            <h1>
+                <p>검색 시 등록 매물을</p>
+                <p>최상단에 노출할 수 있는 광고 상품</p>
+            </h1>
+            <p>집이지 검색결과 가장 상단에 보유 매물이 노출되는 상품입니다.</p>
+            <p>부동산 정보와 함께 노출됩니다.</p>
+        </div>
+    </div>
+    <div>
+        
+    </div>
+</div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.productForm-box {
+    width: 100%;
+    height: 540px;
+    border-top: 1px solid rgb(228, 228, 228);
+}
+
+
+.productForm-box > div {
+    background-image: url("https://pro.dabangapp.com/static/media/intro.8e198009.png");
+    background-size: 394px 442px;
+    background-position: 700px 98px;
+    background-repeat: no-repeat;
+    width: 1200px;
+    height: 100%;
+    margin: 0px auto;
+}
+
+.advertisement-box {
+    width: 100%;
+    height: 540px;
+}
+
+.advertisement-box > div {
+    background-image: url("https://pro.dabangapp.com/static/media/app.84cf24a1.png");
+    background-size: 394px 442px;
+    background-position: 700px 98px;
+    background-repeat: no-repeat;
+    width: 1200px;
+    height: 100%;
+    margin: 0px auto;
+}
+
+</style>
