aa8fcbe687e80a51dee2e123e127025aafb9d089
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-06-18 21:32:59 +0900

    added find email modal function

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -12,7 +12,8 @@
               aria-label="Close"
             ></button>
           </div>
-          <div>
+
+          <div v-if="checkStatus == null">
             <div
               class="d-flex w-75 mx-auto justify-content-center align-items-center"
             >
@@ -71,7 +72,9 @@
               </div>
             </form>
             <div class="d-flex justify-content-evenly mt-2 findIdPassword">
-              <span>아이디 찾기</span>
+              <span class="find-email-btn" @click="checkUserEmail"
+                >아이디 찾기</span
+              >
               <span>비밀번호 찾기</span>
             </div>
             <div class="d-grid gap-2 w-75 mx-auto mt-3 joinMemberBtn">
@@ -88,6 +91,72 @@
               </router-link>
             </div>
           </div>
+          <div v-if="checkStatus === 'email'">
+            <div
+              class="d-flex w-75 mx-auto justify-content-center align-items-center"
+            >
+              <img
+                class="col-3 me-3"
+                src="../../public/matdongsan_logo.png"
+                width="50"
+              />
+              <div class="mt-4 logo-box">
+                <h2 class="nav-title mb-1 fw-bold text-warning fs-2">
+                  Matdongsan
+                </h2>
+                <p class="sub-title m-0 text-center fw-bold">부동산 맛집</p>
+              </div>
+            </div>
+            <form
+              class="d-flex flex-column w-75 mx-auto mt-4"
+              v-on:submit.prevent="handleSubmit"
+            >
+              <div class="">
+                <div>
+                  <h4 class="fs-6 fw-bold">이름</h4>
+                </div>
+                <div class="loginInputBox">
+                  <input
+                    class="h-100 w-100 p-3"
+                    type="text"
+                    name="email"
+                    placeholder="이름 입력"
+                    v-model="user.name"
+                  />
+                </div>
+              </div>
+              <div class="mt-3">
+                <div>
+                  <h4 class="fs-6 fw-bold">전화번호</h4>
+                </div>
+                <div class="loginInputBox">
+                  <input
+                    class="h-100 w-100 p-3"
+                    type="text"
+                    name="email"
+                    placeholder="전화번호 입력"
+                    v-model="user.phone"
+                  />
+                </div>
+              </div>
+              <div class="mt-3">
+                <button
+                  type="submit"
+                  class="w-100 btn btn-warning"
+                  :disabled="!checkFindEmailData"
+                >
+                  아이디 찾기
+                </button>
+                <button
+                  type="button"
+                  class="w-100 mt-2 btn btn-outline-dark"
+                  @click="cancelUserData"
+                >
+                  취소
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
@@ -95,12 +164,14 @@
 </template>
 
 <script setup>
-import { Modal } from "bootstrap";
 import { ref, computed } from "vue";
+let checkStatus = ref(null);
 
 let user = ref({
+  name: "",
   email: "",
   password: "",
+  phone: "",
 });
 function handleSubmit() {
   console.log(JSON.parse(JSON.stringify(user.value)));
@@ -109,13 +180,25 @@ const checkData = computed(() => {
   var result = user.value.email !== "" && user.value.password !== "";
   return result;
 });
+const checkFindEmailData = computed(() => {
+  var result = user.value.name !== "" && user.value.phone !== "";
+  return result;
+});
+function checkUserEmail() {
+  checkStatus.value = "email";
+}
+function cancelUserData() {
+  checkStatus.value = null;
+}
 </script>
 
 <style scoped>
 input {
   border: 1px solid rgb(237, 237, 237);
 }
-
+.find-email-btn {
+  cursor: pointer;
+}
 .loginInputBox {
   height: 40px;
 }
