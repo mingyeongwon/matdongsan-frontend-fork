b06f833eb72af2b08b7670f3b47df81c5719457b
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-06-28 19:47:08 +0900

    resized favorite page map

--- src/views/FavoriteProperty/index.vue
+++ src/views/FavoriteProperty/index.vue
@@ -1,42 +1,38 @@
 <template>
-    <div class=" overflow-hidden w-75 mx-auto">
-        <div>
-            <div class="d-flex ps-3 pe-3 pb-3">
-                <div class="property-list-box w-25 h-100">
-                <div class="col mt-3" @click="backToPropertyList">
-                    <PropertyList type="favorite" />
-                </div>
-                
-                
-                </div>
-                <!-- 카카오맵 -->
-                <div class="right-box ms-4 col vh-100" v-if="status">
-                    <KakaoMap page="favorite"/>
-                </div>
-            </div>
+  <div class="overflow-hidden w-75 mx-auto">
+    <div>
+      <div class="d-flex ps-3 pe-3 pb-3">
+        <div class="property-list-box w-25 h-100">
+          <div class="col mt-3" @click="backToPropertyList">
+            <PropertyList type="favorite" />
+          </div>
         </div>
+        <!-- 카카오맵 -->
+        <div class="right-box ms-4 col map-box p-3" v-if="status">
+          <KakaoMap page="favorite" />
+        </div>
+      </div>
     </div>
-  </template>
-  
-  <script setup>
-  import PropertyList from "@/components/Property/PropertyList.vue";
-  import KakaoMap from "@/components/KakaoMap.vue";
-  import { ref } from "vue";
+  </div>
+</template>
 
-    let status = ref(true);
+<script setup>
+import PropertyList from "@/components/Property/PropertyList.vue";
+import KakaoMap from "@/components/KakaoMap.vue";
+import { ref } from "vue";
 
-    function backToPropertyList() {
-    status.value = !status.value;
-    }
-  </script>
-  
-  <style scoped>
-  
-  
-  .right-box {
-    width: 800px;
-    }
-  
+let status = ref(true);
 
-  </style>
-  
+function backToPropertyList() {
+  status.value = !status.value;
+}
+</script>
+
+<style scoped>
+.right-box {
+  width: 800px;
+}
+.map-box {
+  height: 708px;
+}
+</style>
