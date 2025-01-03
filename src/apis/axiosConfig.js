331ac650e0d70e735ae4002a5b06c06e3648cbae
mingyeongwon <wonmingy@gmail.com>
2025-01-04 03:54:04 +0900

    fix: .env 환경변수에 추가

--- src/apis/axiosConfig.js
+++ src/apis/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://api.matdongsan.org/api";
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
 
 function addAuthHeader(accessToken) {
     axios.defaults.headers.common["Authorization"] = "Bearer " + accessToken;
